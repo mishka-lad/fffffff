a7f219d7cd7ed5fc92f2d6b6d3b48a530cbe86d7
mishka-lad <mishka.marochkin.1996@mail.ru>
2024-06-22 14:29:28 +1000

    Add files via upload

---
+++ resolve1.py
@@ -0,0 +1,32 @@
+r = 'raccoon'
+y = len(r)
+print('Длина слова=',y)
+
+
+first = 7
+second = 4
+summa = first + second
+summa = 11
+diff = first - second
+diff = 3
+print('сумма=',summa,'разность=', diff)
+
+
+
+first = 5
+second = 10
+third = 3
+mean = int((first + second + third)/3)
+print('Среднее арифметическое=', mean)
+
+first_string = 'вторник'
+second_string = 'понедельник'
+print(second_string + ',', first_string)
+
+
+a = 8
+b = 5
+c = 6
+f = (a * b) + (a * c)
+g = (f ** 3)/2
+print('ответ=',g)
